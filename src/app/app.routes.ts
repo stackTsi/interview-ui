8032cd8b65518bdd7bf21ab9505318366c0826b0
Kai Trinh <kai.t@itcgroup.io>
2025-06-11 16:50:19 +0700

    push test on another account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-      { path: 'login', component: Login},
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  {path: 'login', component: Login},
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  // {path: 'home', component: Home},
+  // {path: '', redirectTo: 'home',pathMatch: 'full'}
 ];
